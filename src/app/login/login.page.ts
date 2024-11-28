11ec19bbcd3fcffca68f66be0b28f2dcd861ba39
[Marek] <[maronatwo@gmail.com]>
2024-11-28 14:48:25 +0100

    dodadnie actionSheet na stronie logowania

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +11,36 @@ export class LoginPage implements OnInit {
 
   public title!: string;
   private activatedRoute = inject(ActivatedRoute);
+  public actionSheetCtrl = inject(ActionSheetController);
   constructor() { }
 
   ngOnInit() {
     // pobranie parametru 'title' z queryParams
     this.title = this.activatedRoute.snapshot.queryParamMap.get('title') as string;
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => console.log('Destructive clicked'),
+          },
+        {
+          text: 'Archive',
+          handler: () => console.log('Archive clicked'),
+          },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => console.log('Cancel clicked'),
+          },
+      ],
+    });
+
+    await actionSheet.present();
   }
 
 }
